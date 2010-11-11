e9763a985e7f68f9fdbb0d16e8f2184c844730e6
Mick Killianey <mkgithub@killianey.com>
2010-11-11 08:06:18 +0000

    Rename battleship.go to board.go, since it's just the Board object

--- pkg/battleship/board_test.go
+++ pkg/battleship/board_test.go
@@ -15,25 +15,25 @@ func doSolve(t *testing.T, board *Board) {
     }
 }
 
-func TestSolveBoard0(t *testing.T) {
+func TestBoard_SolveBoard0(t *testing.T) {
     doSolve(t, Board0())
 }
 
-func TestSolveBoard1(t *testing.T) {
+func TestBoard_SolveBoard1(t *testing.T) {
     doSolve(t, Board1())
 }
 
-func TestSolveBoard2(t *testing.T) {
+func TestBoard_SolveBoard2(t *testing.T) {
     doSolve(t, Board2())
 }
 
-func BenchmarkSolveBoard0(b *testing.B) {
+func BenchmarkBoard_SolveBoard0(b *testing.B) {
     for i := 0; i < b.N; i++ {
         Board0().Solve()
     }
 }
 
-func BenchmarkSolveBoard1(b *testing.B) {
+func BenchmarkBoard_SolveBoard1(b *testing.B) {
     for i := 0; i < b.N; i++ {
         Board1().Solve()
     }
